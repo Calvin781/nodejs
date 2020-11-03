e48c7ff0070e0df2958b8b22acaf896f867b6aa9
Calvin781 <calvin.jitnaree123@gmail.com>
2020-11-03 16:34:01 +0100

    search bar

---
+++ react-udemy/pics/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import SearchBar from './SearchBar';
+
+
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+    render() {
+        return <div
+            className="ui container"
+            style={{ marginTop: '10px' }}>
+            <SearchBar onSubmit= {this.onSearchSubmit}></SearchBar>
+        </div>;
+    }
+}
+
+export default App;
