23a2887ca840d6a7c8882df283fa016566ac9b0d
Calvin781 <calvin.jitnaree123@gmail.com>
2020-11-05 11:36:42 +0100

    serch images app

--- react-udemy/pics/src/components/ImageList.js
+++ react-udemy/pics/src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import './ImageList.css';
+import ImageCard from './ImageCard';
 const ImageList = (props) => {
-    const images = props.images.map(({id,urls, alt_description}) => {
-        return <img key={id} src={urls.regular} alt ={alt_description} ></img>
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image}></ImageCard>
     });
 
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
